30d223f11c61b4cbed3a5e7e13fd0da8d802b368
roque <gustavo.roque@grupofolha.com.br>
2017-05-29 15:50:03 -0300

    Criação e atualização de categorias

---
+++ app/Http/Controllers/Api/CategoriesController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace SON\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+
+use SON\Http\Controllers\Controller;
+use SON\Http\Requests;
+use Prettus\Validator\Contracts\ValidatorInterface;
+use Prettus\Validator\Exceptions\ValidatorException;
+use SON\Http\Requests\CategoryRequest;
+use SON\Repositories\CategoryRepository;
+
+
+class CategoriesController extends Controller
+{
+
+    /**
+     * @var CategoryRepository
+     */
+    protected $repository;
+
+    public function __construct(CategoryRepository $repository )
+    {
+        $this->repository = $repository;
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->repository->all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  CategoryRequest $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CategoryRequest $request)
+    {
+        $category = $this->repository->create($request->all());
+
+        return response()->json($category,201);
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return $this->repository->find($id);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  CategoryRequest $request
+     * @param  string            $id
+     *
+     * @return Response
+     */
+    public function update(CategoryRequest $request, $id)
+    {
+        $category = $this->repository->update($request->all(), $id );
+
+        return response()->json($category,200);
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $deleted = $this->repository->delete($id);
+
+        if (request()->wantsJson()) {
+
+            return response()->json([
+                'message' => 'Category deleted.',
+                'deleted' => $deleted,
+            ]);
+        }
+
+        return redirect()->back()->with('message', 'Category deleted.');
+    }
+}
---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SON\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //Faz a dependecy injection automaticamente
+        $this->app->bind(\SON\Repositories\CategoryRepository::class, \SON\Repositories\CategoryRepositoryEloquent::class);
+        //:end-bindings:
+    }
+}
